a89084f1e622a2f96b663710be07d51b29ec85d7
oksana55555 <vitvitskaja@gmail.com>
2022-11-01 19:06:09 -0400

    Test commit

---
+++ newFicher.py
@@ -0,0 +1,6 @@
+nombre1 = input("Donnez 1er nombre, s'il vous plait: ")
+nombre2 = int(input("Donnez 2eme nombre, s'il vous plait: "))
+
+print("La sommedes nombres saisis est :", int(nombre1) + nombre2)
+
+print("La sommedes nombres saisis est :" + str (int(nombre1) + nombre2))
